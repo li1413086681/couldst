efb73c60ae1aa64a649191b2e3b4d8ed91f031d9
liushaoshuaizzz <lssliushaoshuai@126.com>
2019-11-28 15:30:10 +0800

    刘邵帅的前端项目导入

--- test/src/components/main.vue
+++ test/src/components/main.vue
@@ -1,116 +1,51 @@
-<style>
-  .container{
-    width:1366px;
-  }
-  .main{
-     width: 100%;
-  }
-  .login{
-    width: 356px;
-    height: 380px;
-    padding-top: 80px;
-    position: absolute;
-    right: 40px;
-    background-color:#FEDCDA;
-  }
-  #log{
-    background-color: #FD6582;
-    border: none;
-    width: 314px;
-    height: 42px;
-  }
-  .in{
-    width:314px;
-    height:42px;
-  }
-  .tab{
-    font-size: 25px;
-  }
-  .tab:hover{
-    text-decoration: none;
-    color:#337AB7;
-  }
-</style>
-
 <template>
-  <div class="container">
-    <div style="position: absolute;top: 20px;right: 300px;">
-      <a class="tab">用户登录</a>
-    </div>
-    <div style="margin-top:70px;">
-      <hr style="border: solid 1px;" />
-    </div>
-    <div class="main">
-      <div class="login">
-        <span>{{text}}</span><br />
-        <input class="in" placeholder="用户名" v-model="adname" />
-        <br />
-        <br />
-        <span>
-          <input class="in" type="password" placeholder="密码" v-model="adpassword" />
-        </span>
-        <br />
-        <br />
-        <button id="log"  @click="login()">登录</button>
-        <br />
-        <router-link to="/system">入口</router-link>
-        <router-link to="/register">注册</router-link>
-      </div>
-    </div>
-  </div>
-
+	<div class="container" style="width: 100%;">
+		<top></top><hr />
+		<search></search>
+		<changeimg></changeimg>
+		<imagesinfo></imagesinfo>
+		<a id="return_top" @click="returntop1()">返回顶部</a>
+	</div>
 </template>
 
 <script>
 	import imagesinfo from '@/components/imagesinfo';
-  export default {
-    components:{
+	import top from '@/components/top';
+	import search from '@/components/search';
+	import changeimg from '@/components/changeimg';
+	export default{
+		components:{
 			"imagesinfo":imagesinfo,
+			"top":top,
+			"search":search,
+			"changeimg":changeimg
 		},
-    data: function() {
-      return {
-        adname: "",
-        adpassword: "",
-        text:""
-      }
-    },
-    methods: {
-      login() {
-        var ob = this;
-        var url = "http://192.168.1.103:8087/mgj/mgjstore/login"
-        $.ajax(url, {
-          data: {
-            adname: ob.adname,
-            password: ob.adpassword
-          },
-          method: "post",
-          dataType: "json", //服务器返回json格式数据
-          xhrFields: {
-            "withCredentials": true
-          },
-          success: function(result) {
-            if (result == 0) {
-              ob.$router.push("storeadd")
-
-            }
-            if (result == 1) {
-              ob.text = "用户不存在"
-            }
-            if (result == 2) {
-              ob.text = "密码错误"
-            }
-            if (result == 3) {
-              ob.text = "账号封禁，请联系管理员"
-            }
-
-
-          },
-
-        });
-      }
-    },
-    mounted: function() {
-
-    }
-  }
+		methods:{
+			returntop1(){
+				$(window).scrollTop(0);
+			},
+		}
+		
+	}
+	
 </script>
+
+<style>
+	#return_top{
+		background-color: plum;
+		line-height: 60px;
+		color: white;
+		display: inline-block;
+		height: 60px;
+		width: 60px;
+		border-radius: 50%;
+		text-align: center;
+		position: fixed;
+		top: 90%;
+		right: 5%;
+	}
+	#return_top:hover{
+		cursor: pointer;
+		text-decoration: none;
+	}
+</style>
